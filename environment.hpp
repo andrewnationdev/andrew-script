840ed695e0ef3df6becdc34d8ed7b93545623cd6
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2026-06-19 15:30:20 -0300

    Add files via upload

---
+++ environment.hpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
+#include <algorithm>
+
+extern std::map<std::string, int> variables;
+
+// Guarda variáveis que são strings
+extern std::map<std::string, std::string> str_variables; 
---
+++ examples/principal.as
@@ -0,0 +1,2 @@
+import(teste.as)
+write(x * 9 * pi)
